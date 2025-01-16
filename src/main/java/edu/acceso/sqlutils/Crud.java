182a724ca4c6fba6ddd94a20169cfff26947ee0c
José Miguel Sánchez Alés <sio2sio2@gmail.com>
2025-01-16 22:39:50 +0100

    Añade clausulas throws a los métodos declarados en Crud.

--- src/main/java/edu/acceso/sqlutils/Crud.java
+++ src/main/java/edu/acceso/sqlutils/Crud.java
@@ -5,22 +5,22 @@ import java.util.stream.Stream;
 
 public interface Crud<T extends Entity> {
 
-    public Optional<T> get(int id);
-    public Stream<T> get();
+    public Optional<T> get(int id) throws DataAccessException;
+    public Stream<T> get() throws DataAccessException;
 
-    public boolean delete(int id);
-    default boolean delete(T obj) {
+    public boolean delete(int id) throws DataAccessException;
+    default boolean delete(T obj) throws DataAccessException {
         return delete(obj.getId());
     }
 
-    public void insert(T obj);
-    default void insert(Iterable<T> objs) {
+    public void insert(T obj) throws DataAccessException;
+    default void insert(Iterable<T> objs) throws DataAccessException {
         for(T obj: objs) insert(obj);
     }
 
-    public boolean update(T obj);
-    public boolean update(int oldId, int newId);
-    default boolean update(T obj, int newId) {
+    public boolean update(T obj) throws DataAccessException;
+    public boolean update(int oldId, int newId) throws DataAccessException;
+    default boolean update(T obj, int newId) throws DataAccessException {
         return update(obj.getId(), newId);
     }
 }
